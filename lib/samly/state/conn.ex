b927cdadc7976ec33734feefd1512888c51fc031
George Murphy <gmurphy@decisiv.com>
2018-11-25 19:43:29 -0600

    [MICONCALL-323] Update docs

--- lib/samly/state/conn.ex
+++ lib/samly/state/conn.ex
@@ -1,4 +1,10 @@
 defmodule Samly.State.Conn do
+  @moduledoc """
+  Implementation of the state behaviour that stores the SAML assertion directly in the `Plug.Conn.Session`. This allows the assertion state to be
+  shared with other application nodes in a stateless fashion.
+
+  If the Plug.Conn.Session is cookie based, this will add to the size of the cookie header and should be monitored.
+  """
   @behaviour Samly.State
 
   import Plug.Conn
