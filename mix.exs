228dc5e180701415f5f9f55ac3b819ca81ba9ec5
Marcos Tapajos <mtapajos@decisiv.com>
2019-01-10 09:34:04 -0200

    Log out instead of raising an exception

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Samly.Mixfile do
   use Mix.Project
 
-  @version "1.0.0"
+  @version "1.1.0"
   @description "SAML SP SSO made easy - Decisiv fork"
   @source_url "https://github.com/Decisiv/samly"
 
