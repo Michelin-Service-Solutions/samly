7727020563e1dcd9ed632ea8b8853b0f5610f04a
Sandeep Chary <E001770@adgroup.michelin.com>
2020-07-30 11:58:06 -0400

    update source url path and org

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Samly.Mixfile do
 
   @version "1.1.0"
   @description "SAML SP SSO made easy - Decisiv fork"
-  @source_url "https://github.com/Decisiv/samly"
+  @source_url "https://github.com/Michelin-Service-Solutions/samly"
 
   def project() do
     [
@@ -50,6 +50,7 @@ defmodule Samly.Mixfile do
 
   defp package() do
     [
+      organization: "michelin_service_solutions",
       maintainers: ["handnot2"],
       files: ["config", "lib", "LICENSE", "mix.exs", "README.md"],
       licenses: ["MIT"],
