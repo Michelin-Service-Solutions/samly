4e7f9ca8858596198e03f9a688c196d3f4bd1820
sandeep-chary <63057668+sandeep-chary@users.noreply.github.com>
2021-02-10 13:12:40 -0700

    bump version to 1.3.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Samly.Mixfile do
   use Mix.Project
 
-  @version "1.2.0"
+  @version "1.3.0"
   @description "SAML SP SSO made easy - Decisiv fork"
   @source_url "https://github.com/Michelin-Service-Solutions/samly"
 
